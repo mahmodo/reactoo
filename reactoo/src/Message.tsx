1ffe74d5e444ba892963f86b0986f5a01c51c0e7
mahmodo <1997mohammadmahmoud@gamil.com>
2025-04-06 15:24:55 +0200

    start adding Messaage component

---
+++ reactoo/src/Message.tsx
@@ -0,0 +1,9 @@
+// PascalCasing
+function Message() {
+    // jsx: javaScript XML
+    const name='Hamada'
+    if(name)
+        return <h1>hello {name}</h1>;
+    return <h1>hello World</h1>
+}
+export default Message;
